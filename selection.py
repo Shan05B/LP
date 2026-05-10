53c3a35f8c354db54f2239f913fe0321fe5d2b59
Shan05B <shantanu.barge_comp23@pccoer.in>
2026-05-10 18:38:13 +0530

    Add selection sort implementation in selection.py
    Implement selection sort algorithm to sort an array.

---
+++ selection.py
@@ -0,0 +1,12 @@
+arr=list(map(int ,input("Enter the numbers :").split()))
+n=len(arr)
+
+for i in range(n):
+    min_index=i
+    for j in range(i+1,n):
+        if arr[j]<arr[min_index]:
+            min_index=j 
+            
+    arr[i],arr[min_index]=arr[min_index],arr[i]
+print("Sorted array: ")
+print(arr)
